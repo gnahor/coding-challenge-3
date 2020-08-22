ed7af924d941a8171616f86667736b4ec6307826
gnahor <70074377+gnahor@users.noreply.github.com>
2020-08-22 21:49:46 +0200

    My Coding Challenge Submission

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.IntPredicate;
+import java.util.stream.IntStream;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to the SAP Community Coding Challenge #3");
-    }
+    	System.out.printf( removeJoiner( "👩‍💻" ) + "\n" );
+        System.out.printf( removeJoiner( "👨‍👩‍👧‍👦" ) + "\n" );
 
-    public String removeJoiner(String emoji) {
-        //TODO
-        return "";
+    	System.out.printf( addJoiner( "👩💻" ) + "\n" );
+    	System.out.printf( addJoiner( "👨👧" ) + "\n" );
     }
-
-    public String addJoiner(String emojis) {
-        //TODO
-        return "";
+    
+    public static String removeJoiner( String emoji ){
+        IntPredicate zwjFilter = (codePoint) -> { return codePoint == 0x200D ? true : false; };
+        IntStream emojiCodePointsFiltered = emoji.codePoints().filter( zwjFilter.negate() );
+    	
+        return emojiCodePointsFiltered
+        		.collect (
+        		        StringBuilder :: new, 
+        				StringBuilder :: appendCodePoint, 
+        				StringBuilder :: append )
+        		.toString();
     }
+    
+    public static String addJoiner( String emojis ){
+    	List<String> emojiList = new ArrayList<String>();
+    	
+    	emojis.codePoints().forEach(cp -> emojiList.add( new StringBuilder().appendCodePoint( cp ).toString() ) );
+    	
+        return String.join("\u200D", emojiList);
+    }     
 }
